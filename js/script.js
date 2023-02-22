3f574e8822e374f33a5ed2a3599695d65bdce85a
gabrielhca <gabrielo.hca@gmail.com>
2023-02-22 17:02:44 -0300

    removendo menu-ativo

--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
-// Menu Ativo
-
-const links = document.querySelectorAll(".header nav a");
-
-function ativarLink(link) {
-  const url = location.href;
-  const href = link.href;
-  if (url.includes(href)) {
-    link.classList.add("ativo");
-  }
-}
-
-links.forEach(ativarLink);
-
 // Animação
-
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
