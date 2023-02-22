82239fa257dcd512cc197f9134a2d934e159fd51
gabrielhca <gabrielo.hca@gmail.com>
2023-02-22 15:46:55 -0300

    scroll suave no portifolio

--- js/script.js
+++ js/script.js
@@ -17,3 +17,23 @@ links.forEach(ativarLink);
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollSuave();
